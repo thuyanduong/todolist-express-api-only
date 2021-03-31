64dce9e80af3bc5404afccef3783939a5b86aec4
Ann Duong <thuyanannduong@gmail.com>
2021-03-31 18:18:03 -0400

    added root message

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.status(200).json({message: "This is an API"})
+})
+
 app.use('/users', usersRouter)
 app.use('/tasks', tasksRouter)
 
